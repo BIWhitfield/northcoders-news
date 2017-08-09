fce462d911d4fe402b919c7b8b40aecbb7a258d9
BIWhitfield <beniwhitfield@gmail.com>
2017-08-09 21:57:38 +0100

    [PostCommentForm] add submit button, add props to text area

--- src/components/PostCommentForm.js
+++ src/components/PostCommentForm.js
@@ -1,19 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const PostCommentForm = props => (
-  <div className="field">
-    <div className="control">
-      <textarea className="textarea" placeholder="Post comment" />
-    </div>
-    <button
-      className="button is-info"
-      type="submit"
-      value="post"
-    >
-    Post comment
-    </button>
+  <div>
+    <form className="media-content" onSubmit={props.submitHandler}>
+      <textarea
+        className="textarea"
+        placeholder="Post comment"
+        value={props.input}
+        onChange={props.inputHandler}
+      />
+      <button
+        className="button is-info"
+        type="submit"
+        value="post"
+      >
+      Post comment
+      </button>
+    </form>
   </div>
     );
 
+PostCommentForm.propTypes = {
+  submitHandler: PropTypes.func.isRequired,
+  inputHandler: PropTypes.func.isRequired,
+  input: PropTypes.string.isRequired,
+};
 
 export default PostCommentForm;
