f5c7e13cf38e8742b355310e93f2bdd33c4a0845
BIWhitfield <beniwhitfield@gmail.com>
2017-08-09 13:09:29 +0100

    [Articlecard] update props Navlink

---
+++ src/components/ArticleCard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+
+const ArticleCard = props => (
+  <div className="box">
+    <article className="media">
+      <div className="media-left">
+        <p>Upvotes:</p>
+        {props.votes}
+      </div>
+      <div className="media-content">
+        <div className="content">
+          <NavLink to={`/articles/${props.id}`} className="title is-3">{props.title}</NavLink>
+        </div>
+      </div>
+    </article>
+  </div>
+  );
+
+ArticleCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  votes: PropTypes.number.isRequired,
+  id: PropTypes.string.isRequired,
+};
+
+export default ArticleCard;
