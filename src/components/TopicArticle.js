f48e8d9fed1e75f3a15f12473c7c6055a4eea91b
BIWhitfield <beniwhitfield@gmail.com>
2017-08-10 18:14:39 +0100

    [TopicArticle] initial commit

---
+++ src/components/TopicArticle.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+
+const TopicArticle = props => (
+  <div className="box">
+    <article className="media">
+      <span className="column is-narrow rows">
+        <a className="is-danger is-small" onClick={props.voteOnTopicArticles.bind(null, props.id, 'up')} >
+          <i className="fa fa-arrow-up row" />
+        </a>
+        <span className="row tag is-medium bold">{props.votes}</span>
+        <a className="is-danger is-small" onClick={props.voteOnTopicArticles.bind(null, props.id, 'down')} >
+          <i className="fa fa-arrow-down row" />
+        </a>
+      </span>
+      <div className="media-content">
+        <div className="content">
+          <NavLink to={`/article/${props.id}`} className="title is-3">{props.title}</NavLink>
+        </div>
+        <div>Created By: {props.article.created_by}</div>
+        <div>Comments: {props.article.comments}</div>
+      </div>
+    </article>
+  </div>
+);
+
+TopicArticle.propTypes = {
+  title: PropTypes.string.isRequired,
+  votes: PropTypes.number.isRequired,
+  id: PropTypes.string.isRequired,
+  article: PropTypes.object.isRequired,
+};
+
+export default TopicArticle;
