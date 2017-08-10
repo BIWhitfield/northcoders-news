88ac2d1b7b0b7ed2a11fd77e1fbc6396a7deb077
Ben <beniwhitfield@gmail.com>
2017-08-10 23:53:36 +0100

    Header style (#8)
    * [Header.css] add styles for title, titleImage and subtitle
    
    * [Header] add northcoders logo
    
    * [ArticleCard] remove css import
    
    * [index.html] link to fonts in header

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
-import '../css/ArticleCard.css';
 
 const ArticleCard = props => (
   <div className="box">
