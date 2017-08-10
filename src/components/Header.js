900a4800b7bca100f751fefa4e8958f68a05d87b
BIWhitfield <beniwhitfield@gmail.com>
2017-08-10 23:52:36 +0100

    [Header] add northcoders logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,9 @@ const Header = () => (
   <section className="hero is-dark bold">
     <div className="hero-body">
       <div className="container">
+        <img className="titleImage" src={'https://northcoders.com/images/logos/learn_to_code_manchester_rw_second.png'} alt="Northcoders logo" />
         <h1 className="title has-text-centered">
-          NORTHCODERS NEWS
+          NEWS
       </h1>
         <h2 className="subtitle has-text-centered">
           Masquerading as fact...
