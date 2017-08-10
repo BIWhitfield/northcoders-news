fde00347a4d644e87eb525641372383149c302ed
BIWhitfield <beniwhitfield@gmail.com>
2017-08-10 14:43:06 +0100

    [VoteButtons] add onClick function passed through props

--- src/components/VoteButtons.js
+++ src/components/VoteButtons.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 
 const VoteButtons = props => (
   <span className="column is-narrow rows">
-    <a className="is-danger is-small" >
+    <a className="is-danger is-small" onClick={props.handleVote.bind(null, props.id, 'up')} >
       <i className="fa fa-arrow-up row" />
     </a>
     <span className="row tag is-medium bold">{props.votes}</span>
-    <a className="is-danger is-small" >
+    <a className="is-danger is-small" onClick={props.handleVote.bind(null, props.id, 'down')} >
       <i className="fa fa-arrow-down row" />
     </a>
   </span>
@@ -15,6 +15,7 @@ const VoteButtons = props => (
 
 VoteButtons.propTypes = {
   votes: PropTypes.number,
+  handleVote: PropTypes.func.isRequired,
 };
 
 export default VoteButtons;
