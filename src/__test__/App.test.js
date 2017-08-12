8ee2befcddc3bce6df946ffe042812d1ab881d64
Ben <beniwhitfield@gmail.com>
2017-08-12 19:16:59 +0100

    Test actions (#11)
    * [package.json] add packages to test components
    
    * [App.test] test render of component
    
    * [actions.test] basic tests - fetchAllArticles,  fetchAllTopicTitles, fetchAllTopicTitles, fetchAllTopicArticles, fetchIndividualArticle, articleComments, fetchAllUsers

---
+++ src/__test__/App.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../components/App';
+
+describe('<App />', () => {
+  it('it renders 1 <App /> component', () => {
+    const component = shallow(<App />);
+    expect(component).toHaveLength(1);
+  })
+  it('renders props correctly', () => {
+    const component = shallow(<App name="app" />);
+    expect(component.instance().props.name).toBe('app');
+  })
+})
