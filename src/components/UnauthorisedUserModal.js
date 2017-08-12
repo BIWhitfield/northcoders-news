b592f8e52d8474aaa8dd4d136a3fff0c80626691
BIWhitfield <beniwhitfield@gmail.com>
2017-08-12 14:19:52 +0100

    [UnauthorisedUserModal] refactor to function

--- src/components/UnauthorisedUserModal.js
+++ src/components/UnauthorisedUserModal.js
@@ -1,31 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class UnauthorisedUserModal extends React.Component {
-  render() {
-    console.log(this.props)
-    if (!this.props.modalOverlay) {
-      return null;
-    }
-    return (
-      <div className="modal is-active">
-        <div className="modal-background" />
-        <div className="modal-content">
-          <section className="box">
-            <div className="column is-8">
-              <h1 className="title"><b>Unauthorised User!</b></h1>
-              <p>You don't have permission to delete this comment</p>
-            </div>
-          </section>
-        </div>
-        <button className="modal-close is-large" aria-label="close" onClick={this.props.onClose}/>
-      </div>
-    );
+const UnauthorisedUserModal = (props) => {
+  if (!props.modalOverlay) {
+    return null;
   }
-}
+  return (
+    <div className="modal is-active">
+      <div className="modal-background" />
+      <div className="modal-content">
+        <section className="box">
+          <div className="column is-8">
+            <h1 className="title"><b>Unauthorised User!</b></h1>
+            <p>You do not have permission to delete this comment</p>
+          </div>
+        </section>
+      </div>
+      <button className="modal-close is-large" aria-label="close" onClick={props.onClose} />
+    </div>
+  );
+};
+
 
 UnauthorisedUserModal.propTypes = {
   modalOverlay: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
 };
 
 export default UnauthorisedUserModal;
