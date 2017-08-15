81dd2a42918aaf06f47e3fa6a029b162eba3dfb4
BIWhitfield <beniwhitfield@gmail.com>
2017-08-15 12:59:57 +0100

    [fetchAllArticles] test async function and actions

--- src/__test__/fetchAllArticles.test.js
+++ src/__test__/fetchAllArticles.test.js
@@ -1,13 +1,20 @@
 import configureMockStore from 'redux-mock-store';
+import axios from 'axios';
+import httpAdapter from 'axios/lib/adapters/http';
 import thunk from 'redux-thunk';
 import nock from 'nock';
 import { fetchArticles } from '../actions/actions';
+import * as actions from '../actions/fetchAllArticles';
 import * as types from '../actions/types';
 import { ROOT } from '../config';
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
+const host = 'http://localhost';
+
+axios.defaults.host = host;
+axios.defaults.adapter = httpAdapter;
 
 describe('async action: FetchAllArticles', () => {
   afterEach(() => {
@@ -24,7 +31,7 @@ describe('async action: FetchAllArticles', () => {
     // Actions we expect to be dispatched
     const expectedActions = [
       { type: types.FETCH_ARTICLES_REQUEST },
-      { type: types.FETCH_ARTICLES_SUCCESS, articles: [1, 2, 3] },
+      { type: types.FETCH_ARTICLES_SUCCESS, data: [1, 2, 3] },
     ];
 
     // create a fake Redux store
@@ -36,5 +43,32 @@ describe('async action: FetchAllArticles', () => {
         // check that all actions are equal to expected actions
         expect(store.getActions()).toEqual(expectedActions);
       });
+  });
+});
+
+describe('fetchAllArticles', () => {
+  test('it is a function', () => {
+    expect(typeof fetchArticles).toBe('function');
+  });
+  test('its request action returns an object', () => {
+    expect(typeof actions.fetchArticlesRequest()).toEqual('object');
+  });
+
+  test('its success action returns an object', () => {
+    expect(typeof actions.fetchArticlesSuccess()).toEqual('object');
+  });
+  test('success action returns data passed as a parameter', () => {
+    const data = [1, 2, 3];
+    const test1 = actions.fetchArticlesSuccess(data);
+    expect(test1.data).toEqual([1, 2, 3]);
+  });
+
+  test('its error action returns an object', () => {
+    expect(typeof actions.fetchArticlesError()).toEqual('object');
+  });
+  test('its error action returns data passed as a parameter', () => {
+    const data = 'There was an Error';
+    const test = actions.fetchArticlesError(data);
+    expect(test.data).toEqual('There was an Error');
   });
 });
